f47380dd920765b5fd76b6492878d9931011a04f
Christopher Carver <vick3434@comcast.net>
2021-07-04 14:00:37 -0500

    Create arrao of characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,10 @@
 // Assignment code here
 
+// Create an array of each type of characters
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var characterArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
